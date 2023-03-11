5d872599a08076d1de069815c8ba2bd48e18c59f
Curtis <curtis@live.fr>
2023-03-11 17:39:40 +0100

    Commit

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "bbdb7c5e3fe102",
-  password: "17e6d54e",
-  database: "heroku_3cf69f851b9a05c",
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b136162cc4410f",
+  password: "fa21a3bb",
+  database: "heroku_f7f74de54416b25",
 });
 db.connect((err) => {
   if (err) throw err;
   console.log("MySQL connected !");
 });
+
+setInterval(() => db.query("SELECT 1"), 30000);
 
 module.exports = db;
