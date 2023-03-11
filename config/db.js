c8266d2199545af07d62ebf57ce3e4bbcdf52a45
Curtis <curtis@live.fr>
2023-03-11 15:47:07 +0100

    Commit

--- config/db.js
+++ config/db.js
@@ -5,10 +5,6 @@ const db = mysql.createConnection({
   user: "bbdb7c5e3fe102",
   password: "17e6d54e",
   database: "heroku_3cf69f851b9a05c",
-  // host: "eu-cdbr-west-03.cleardb.net",
-  // user: "b4c110b57334ed",
-  // password: "53e49e8d",
-  // database: "heroku_159d15e2b75e127",
 });
 db.connect((err) => {
   if (err) throw err;
