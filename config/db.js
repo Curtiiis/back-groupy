1711164e35eecd8fbe338ded6a7c4636f8c0b2f6
Curtis <curtis@live.fr>
2023-03-11 14:51:02 +0100

    [ADD]: Add files for deploy

--- config/db.js
+++ config/db.js
@@ -2,9 +2,13 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection({
   host: "eu-cdbr-west-03.cleardb.net",
-  user: "b4c110b57334ed",
-  password: "53e49e8d",
-  database: "heroku_159d15e2b75e127",
+  user: "bbdb7c5e3fe102",
+  password: "17e6d54e",
+  database: "heroku_3cf69f851b9a05c",
+  // host: "eu-cdbr-west-03.cleardb.net",
+  // user: "b4c110b57334ed",
+  // password: "53e49e8d",
+  // database: "heroku_159d15e2b75e127",
 });
 db.connect((err) => {
   if (err) throw err;
